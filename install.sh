aeb29b952e823f13fca33f422b0f2a726f87a5c3
bladentd <67013093+bladent@users.noreply.github.com>
2024-04-11 22:14:30 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -63,7 +63,7 @@ EOF
 
 
 gen_data() {
-portplus=$((port + 1))
+portplus=$port+1
     seq $FIRST_PORT $LAST_PORT | while read port; do
         echo "$(random)/$(random)$portplus/$IP4/$port/$(gen64 $IP6)"
     done
